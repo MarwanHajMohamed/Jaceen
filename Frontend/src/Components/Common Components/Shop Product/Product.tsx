0a866fbdc59d8287f2322abb2227573d8b85a079
Marwan <2017159@brunel.ac.uk>
2025-02-26 21:23:03 +0000

    Changed Shop Layout

--- Frontend/src/Components/Common Components/Shop Product/Product.tsx
+++ Frontend/src/Components/Common Components/Shop Product/Product.tsx
@@ -12,9 +12,15 @@ export default function Product(props: Props) {
       <div className="image-container">
         <img src={props.img} alt="" />
       </div>
+      <div className="stars">
+        <i className="fa-solid fa-star"></i>
+        <i className="fa-solid fa-star"></i>
+        <i className="fa-solid fa-star"></i>
+        <i className="fa-solid fa-star"></i>
+        <i className="fa-solid fa-star"></i>
+      </div>
       <div className="product-title">{props.title}</div>
       <div className="price">£{props.price}</div>
-      <button>Add to basket</button>
     </div>
   );
 }
