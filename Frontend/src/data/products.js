01c9fc94a68cbd77dd234c77f213d2076ec439cf
Marwan <2017159@brunel.ac.uk>
2025-02-26 20:51:17 +0000

    Added Product Assets

--- Frontend/src/data/products.js
+++ Frontend/src/data/products.js
@@ -1,6 +1,20 @@
 import Alpha from "../assets/alpha.png";
 import Energise from "../assets/energise.png";
 import Shilajit from "../assets/shilajit.jpg";
+import Growth from "../assets/growth.jpg";
+import CBD from "../assets/cbd.jpg";
+import CBDOil from "../assets/cbd_oil.jpeg";
+import Immune from "../assets/immune.png";
+import ImmuneAF from "../assets/immune_alcohol_free.jpeg";
+import Patchouli from "../assets/patchouli.jpeg";
+import Jasmine from "../assets/jasmine.jpeg";
+import PatchJasmine from "../assets/patch_jasmine.jpeg";
+import SkinSerum from "../assets/skin_serum.jpeg";
+import Toning from "../assets/toning.jpeg";
+import HairSerum from "../assets/hair_serum.jpeg";
+import HairSkinToning from "../assets/hair_skin_toning.jpeg";
+import Beginner from "../assets/training-programmes.jpeg";
+import Advanced from "../assets/training_programmes_advanced.jpeg";
 
 export const products = [
   {
@@ -29,97 +43,97 @@ export const products = [
     title: "Growth",
     price: 50,
     category: "Sports Nutrition",
-    img: "",
+    img: Growth,
   },
   {
     id: 5,
     title: "Organic CBD Balm",
     price: 80,
     category: "Sports Nutrition",
-    img: "",
+    img: CBD,
   },
   {
     id: 6,
     title: "Immune",
     price: 35,
     category: "Sports Nutrition",
-    img: "",
+    img: Immune,
   },
   {
     id: 7,
     title: "Immune Alcohol Free",
     price: 30,
     category: "Sports Nutrition",
-    img: "",
+    img: ImmuneAF,
   },
   {
     id: 8,
     title: "Organic CBD Oil",
     price: 60,
     category: "Sports Nutrition",
-    img: "",
+    img: CBDOil,
   },
   {
     id: 9,
     title: "Patchouli Wood Natural Body Roll-on",
     price: 15,
     category: "Skin Care",
-    img: "",
+    img: Patchouli,
   },
   {
     id: 10,
     title: "Royal Jasmine Natural Body Roll-on",
     price: 15,
     category: "Skin Care",
-    img: "",
+    img: Jasmine,
   },
   {
     id: 11,
     title: "Royal Jasmine & Patchouli Wood Body Roll-on Twin Pack",
     price: 25,
     category: "Skin Care",
-    img: "",
+    img: PatchJasmine,
   },
   {
     id: 12,
     title: "Skin Repair Serum",
     price: 55,
     category: "Skin Care",
-    img: "",
+    img: SkinSerum,
   },
   {
     id: 13,
     title: "Toning Solution",
     price: 25,
     category: "Skin Care",
-    img: "",
+    img: Toning,
   },
   {
     id: 14,
     title: "Hair Growth Serum",
     price: 55,
     category: "Hair Care",
-    img: "",
+    img: HairSerum,
   },
   {
     id: 15,
     title: "Hair Growth Serum, Toning Solution and Skin Repair Serum Trio",
     price: 110,
     category: "Hair Care",
-    img: "",
+    img: HairSkinToning,
   },
   {
     id: 16,
     title: "8 Week Beginner Strength and Hypertrophy Programme",
     price: 30,
     category: "Training Programmes",
-    img: "",
+    img: Beginner,
   },
   {
     id: 17,
     title: "8 Week Advanced Strength and Hypertrophy Programme",
     price: 40,
     category: "Training Programmes",
-    img: "",
+    img: Advanced,
   },
 ];
