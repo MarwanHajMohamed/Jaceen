c0d91762303ed052dc027889af3c512a8c83cc0f
Marwan <2017159@brunel.ac.uk>
2025-02-26 21:25:59 +0000

    Added Scroll to Top when Navigating

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -3,11 +3,13 @@ import Navbar from "./Components/Navbar/Navbar";
 import Landing from "./Pages/Landing/Landing";
 import Footer from "./Components/Footer/Footer";
 import Category from "./Components/Category/Category";
+import ScrollToTop from "./Components/ScrollToTop";
 
 function App() {
   return (
     <>
       <Router>
+        <ScrollToTop />
         <Navbar />
         <Routes>
           <Route path="/" element={<Landing />} />
